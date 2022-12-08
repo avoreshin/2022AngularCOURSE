0b8e50c7c307e5be6e25012f4196dd80df76399e
avoreshin <avoreshin@gmail.com>
2022-12-08 23:11:14 +0300

    feat: tasks mockData & interface

---
+++ todo/src/app/mock-task.ts
@@ -0,0 +1,29 @@
+import {Task} from "./Task"
+
+export const TASKS : Task [] = [
+  {
+    id:1,
+    text: 'Doctors Apple',
+    day: 'Dec 8th at 10:48pm',
+    reminder: true
+  },
+  {
+    id:2,
+    text: 'Doctors Apple',
+    day: 'Dec 8th at 10:50pm',
+    reminder: true
+  },
+  {
+    id:3,
+    text: 'Meeting at School',
+    day: 'Dec 6th at 0:15pm',
+    reminder: false
+  },
+  {
+    id:4,
+    text: 'Food Shopping',
+    day: 'Nov 25th at 8:30am',
+    reminder: true
+  }
+
+]
