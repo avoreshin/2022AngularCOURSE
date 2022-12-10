2710e88c91de061bf74a56bd58caed02d0d1a6ab
avoreshin <avoreshin@gmail.com>
2022-12-11 00:07:32 +0300

    feat: add toogleReminder

--- todo/src/app/components/tasks/tasks.component.ts
+++ todo/src/app/components/tasks/tasks.component.ts
@@ -28,6 +28,6 @@ export class TasksComponent {
   }
   toggleReminder(task: Task){
     task.reminder = !task.reminder
-    console.log(task.reminder)
+    this.taskService.updateTaskReminder(task).subscribe()
   }
 }
