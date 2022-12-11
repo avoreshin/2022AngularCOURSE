f5881ab6adde59e7089d686e22b45c6c4f85d9fa
avoreshin <avoreshin@gmail.com>
2022-12-11 16:43:22 +0300

    feat: add toggleAddTask

--- todo/src/app/components/header/header.component.ts
+++ todo/src/app/components/header/header.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UiService} from "../../services/ui.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   title: string = 'Task-Tracker';
+  showAddTask: boolean = true;
+  subscription: Subscription | undefined;
 
-  constructor() {
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe(Value => this.showAddTask = Value)
   }
 
-  toggleAddTask(){
+  toggleAddTask() {
+    this.uiService.toggleAddTask()
+  }
 
+  ngOnInit(): void {
   }
 }
