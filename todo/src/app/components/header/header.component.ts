b0ebdf55017a42f0583b674ca8b648fcc1b16df4
avoreshin <avoreshin@gmail.com>
2022-12-11 17:28:25 +0300

    feat: footer

--- todo/src/app/components/header/header.component.ts
+++ todo/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UiService} from "../../services/ui.service";
 import {Subscription} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = true;
   subscription: Subscription | undefined;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router:Router) {
     this.subscription = this.uiService
       .onToggle()
       .subscribe(Value => this.showAddTask = Value)
@@ -20,6 +21,10 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask() {
     this.uiService.toggleAddTask()
+  }
+
+  hasRouter(router: string){
+    return this.router.url === router;
   }
 
   ngOnInit(): void {
